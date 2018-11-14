e2923effb871998ff144fd57dba339880943e56c
Ross Salge <rossnsalge@gmail.com>
2018-11-13 19:54:17 -0600

    Ben's favorite color

--- src/App.js
+++ src/App.js
@@ -6,20 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="header">Testing</div>
+        <div className="footer">Testing</div>
+        <div className="content">
+          <h1>  Hi Ben! </h1>
+        Testing
+        
+        </div>
+        <div className="sidebar-left">Testing</div>
+        <div className="sidebar-right">Testing</div>
       </div>
     );
   }
