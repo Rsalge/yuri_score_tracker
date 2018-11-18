0b7536dd227e33eceaa5f20c5b3475fbf8b2f4e7
Ross Salge <rossnsalge@gmail.com>
2018-11-18 16:40:47 -0600

    Setup basic node server with one endpoint

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.getPing();
+  }
+  getPing() {
+    axios.get("/ping").then(response => {
+      console.log(response.data);
+    });
+  }
   render() {
     return (
       <div className="App">
         <div className="header">Testing</div>
         <div className="footer">Testing</div>
         <div className="content">
-          <h1>  Hi Ben! </h1>
-        Testing
-        
+          <h1> Hi Ben! </h1>
+          Testing
         </div>
         <div className="sidebar-left">Testing</div>
         <div className="sidebar-right">Testing</div>
